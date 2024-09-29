fd7aa7f5eb141b13f9ce9412f7417fb3055470a1
Simin Cao <simin.cao@outlook.com>
2024-09-29 14:44:42 +0200

    hide fab under print mode

--- components/Header.tsx
+++ components/Header.tsx
@@ -11,87 +11,86 @@ const Header = () => {
     const ref = createRef<HTMLDivElement>();
 
     const isVisible = useOnScreen(ref);
-    const [isFirstLoad,setIsFirstLoad] = useState(true);
+    const [isFirstLoad, setIsFirstLoad] = useState(true);
     useEffect(() => {
         if (typeof window !== "undefined") {
-            window.addEventListener("scroll", () =>
-            {
-                if(window.scrollY>document.getElementById('main-header')!.scrollHeight){
+            window.addEventListener("scroll", () => {
+                if (window.scrollY > document.getElementById('main-header')!.scrollHeight) {
                     setIsFirstLoad(false);
                 }
             });
         }
-      }, [isVisible]);
+    }, [isVisible]);
 
 
     return (
         <>
             <Container ref={ref} id='main-header'>
-                <div className={styles.top}/>
-                <Grid sx={{ml:{xs:2, md:5}, pt:5, pb:3 , display:'flex'}} container spacing={1}>
+                <div className={styles.top} />
+                <Grid sx={{ ml: { xs: 2, md: 5 }, pt: 5, pb: 3, display: 'flex' }} container spacing={1}>
                     <Grid item md={4}>
-                        <Avatar 
-                        variant='square'
-                        className={styles.avatar}
-                        src={(avatarsimin as any).src} alt='avatar'/>
+                        <Avatar
+                            variant='square'
+                            className={styles.avatar}
+                            src={(avatarsimin as any).src} alt='avatar' />
                     </Grid>
                     <Grid item md={8}>
-                        <Typography 
-                        variant="h1" 
-                        className={styles.name}>
+                        <Typography
+                            variant="h1"
+                            className={styles.name}>
                             {me.name}
                         </Typography>
                     </Grid>
                 </Grid>
 
                 <Grid container spacing={0}>
-                    <Grid item xs={12} sx={{display:{xs:'block', md: 'none'}}}>
+                    <Grid item xs={12} sx={{ display: { xs: 'block', md: 'none' } }}>
                         <Typography className={styles.bio}>
                             {me.bio}
                         </Typography>
                     </Grid>
                     <Grid item sm={4}>
-                        <List sx={{ml:5, mr:7}} className={styles.pi}>
-                            {me.personal_info.map((pi,i) => (
-                                <Partials.PersonalInfo key={pi.name+i} {...pi} />
+                        <List sx={{ ml: 5, mr: 7 }} className={styles.pi}>
+                            {me.personal_info.map((pi, i) => (
+                                <Partials.PersonalInfo key={pi.name + i} {...pi} />
                             ))}
                         </List>
-                        <Partials.Languages languages={me.languages}/>
+                        <Partials.Languages languages={me.languages} />
                     </Grid>
-                    <Grid item sm={8} sx={{display:{xs:'none', md:'block'}}}>
+                    <Grid item sm={8} sx={{ display: { xs: 'none', md: 'block' } }}>
                         <Typography className={styles.bio}>
                             {me.bio}
                         </Typography>
                     </Grid>
                 </Grid>
             </Container>
-            <Grid className={`${styles['header-sm']} ${isVisible?styles['header-sm-disappear']:styles['header-sm-appear']}`} 
-                    sx={{pt:2,w:100, position:'fixed', top:0, ml:0, mr:0, display:(!isFirstLoad?'flex':'none'), justifyContent: 'center'}} 
-                    container 
-                    spacing={2}>
+            <Grid className={`${styles['header-sm']} ${isVisible ? styles['header-sm-disappear'] : styles['header-sm-appear']}`}
+                sx={{ pt: 2, w: 100, position: 'fixed', top: 0, ml: 0, mr: 0, display: (!isFirstLoad ? 'flex' : 'none'), justifyContent: 'center' }}
+                container
+                spacing={2}>
                 <Grid item md={2}>
-                    <Avatar 
-                    variant='square'
-                    className={styles['avatar-sm']}
-                    src={(avatarsimin as any).src} alt='avatar'/>
+                    <Avatar
+                        variant='square'
+                        className={styles['avatar-sm']}
+                        src={(avatarsimin as any).src} alt='avatar' />
                 </Grid>
                 <Grid item md={3}>
-                    <Typography 
-                    variant="h2" 
-                    className={styles['name-sm']}>
+                    <Typography
+                        variant="h2"
+                        className={styles['name-sm']}>
                         {me.name}
                     </Typography>
                 </Grid>
                 <Grid item md={1} className={styles['pi-small']}>
-                {me.personal_info.map((pi,i) => (
-                    (pi.name==='email'|| pi.name==='tel')?
-                        <Typography key={pi.name+i}> {pi.value}</Typography>
-                        :''
-                ))}
+                    {me.personal_info.map((pi, i) => (
+                        (pi.name === 'email' || pi.name === 'tel') ?
+                            <Typography key={pi.name + i}> {pi.value}</Typography>
+                            : ''
+                    ))}
                 </Grid>
             </Grid>
             <Tooltip title="email me">
-                <Fab className={styles['email-fab']} href={me.personal_info.filter(pi=>pi.name==='email')[0]['href']}>
+                <Fab className={styles['email-fab']} href={me.personal_info.filter(pi => pi.name === 'email')[0]['href']}>
                     <FontAwesomeIcon icon={faAt} />
                     <FontAwesomeIcon icon={faAt} className={styles['email-effect']} />
                 </Fab>
